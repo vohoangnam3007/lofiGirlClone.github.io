1758d4f9742b0d33d66d15382ce94e01baf3d429
HoangNam2801 <56285207+vohoangnam3007@users.noreply.github.com>
2022-01-21 11:27:52 +0700

    Update main.js

--- main.js
+++ main.js
@@ -28,5 +28,11 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  smoothscroll.polyfill();
 }
+
+import smoothscroll from 'smoothscroll-polyfill';
+// or if linting/typescript complains
+import * as smoothscroll from 'smoothscroll-polyfill';
+
+// kick off the polyfill!
+smoothscroll.polyfill();
