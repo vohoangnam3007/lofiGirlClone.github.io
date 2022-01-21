1dd2763377c592d74dc4d5d9553034ed1e1c9241
HoangNam2801 <56285207+vohoangnam3007@users.noreply.github.com>
2022-01-21 11:29:24 +0700

    Update main.js

--- main.js
+++ main.js
@@ -24,15 +24,19 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function SmoothVerticalScrolling(e, time, where) {
+  var eTop = e.getBoundingClientRect().top;
+  var eAmt = eTop / 100;
+  var curTime = 0;
+  while (curTime <= time) {
+      window.setTimeout(SVS_B, curTime, eAmt, where);
+      curTime += time / 100;
+  }
 }
 
-import smoothscroll from 'smoothscroll-polyfill';
-// or if linting/typescript complains
-import * as smoothscroll from 'smoothscroll-polyfill';
-
-// kick off the polyfill!
-smoothscroll.polyfill();
+function SVS_B(eAmt, where) {
+  if(where == "center" || where == "")
+      window.scrollBy(0, eAmt / 2);
+  if (where == "top")
+      window.scrollBy(0, eAmt);
+}
