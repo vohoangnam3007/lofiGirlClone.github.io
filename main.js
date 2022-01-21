dc1b1edfb908e4ba2b0e1621888b394ddc66ce1d
HoangNam2801 <56285207+vohoangnam3007@users.noreply.github.com>
2022-01-21 11:24:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -28,4 +28,5 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  smoothscroll.polyfill();
 }
